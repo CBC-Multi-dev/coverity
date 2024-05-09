b318e0a8bc2fdba9a81f6e2af167d0539380a83f
CBCChitra <107122596+CBCChitra@users.noreply.github.com>
2024-05-09 13:47:56 +0530

    Update VulnadoApplicationTests.java

--- src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
+++ src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class VulnadoApplicationTests {
-
+  app.secret=edf10572-880c-4dd9-aaf0-6ec402f678db
 	@Test
 	public void contextLoads() {
 	}
